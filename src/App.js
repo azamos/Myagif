4839317d922c166265458e84f56c05ee9c667c8b
amos zohar <azamoszohar@gmail.com>
2023-05-01 06:55:13 +0300

    added week

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.png';
+
 import './App.css';
 import Menu from './Menu';
 import CONSTANTS from './Menu/appConstants';
 import TasksPage from './TasksPage';
 import { useState } from 'react';
+import Week from './Sched/Week';
 
 const {APP_PAGES} = CONSTANTS;
 const {TASK_PAGE,SCHED_PAGE,STATS_PAGE,SOCIAL_PAGE} = APP_PAGES;
@@ -19,8 +20,8 @@ const ActiveComponent = props => {
 function App() {
   return (
     <div className="App">
-      <img src={logo} height={"300px"}/>
       <Menu/>
+      <Week/>
     </div>
   );
 }
--- src/HomePags/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Menu from "../Menu";
-
-const HomePage = () => <div><Menu/></div>;
-
-export default HomePage;
